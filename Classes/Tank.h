1335a2ef16aba34601d077717d19cdcef0a24767
gcx-0ss <1159147015@qq.com>
2024-07-04 16:29:26 +0800

    Add files via upload
    坦克类

---
+++ Classes/Tank.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef __TANK_H__
+#define __TANK_H__
+#include "cocos2d.h"
+using namespace cocos2d;
+#include "Bullet.h"
+class Tank :public Sprite {
+public:
+    static Tank* create(const std::string& TankImage);
+    virtual void update(float delta);
+    void moveleft();
+    void moveright();
+    void moveup();
+    void movedown();
+    void stopmoving();
+    bool isReachBoundry();
+    void deletetank();
+   
+private:
+    Vec2 vec;
+    Vec2 Tank_Current_Postion;
+    int Lives;
+    //Bullet* bullet;
+
+};
+#endif
